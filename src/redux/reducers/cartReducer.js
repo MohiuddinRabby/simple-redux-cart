d73b7d4632d47226ec7c066e640ad7fb8dfc535d
rabbymohiuddin <rabbymohiuddin@gmail.com>
2020-10-17 13:00:37 +0600

    cartReducer created

--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -1,9 +1,19 @@
+import { ADD_TO_CART, REMOVE_FROM_CART } from "../actions/cartAction";
+
 const initState = {
   cart: [],
 };
 
-const CartReducer = (state = initState, action) => {
+export const CartReducer = (state = initState, action) => {
   switch (action.type) {
+    case ADD_TO_CART:
+      const newProductID = action.id;
+      const newCart = [...state.cart, newProductID];
+      return { cart: newCart };
+    case REMOVE_FROM_CART:
+      const removedID = action.id;
+      const remaingCart = state.cart.filter((item) => item !== removedID);
+      return { cart: remaingCart };
     default:
       return state;
   }
